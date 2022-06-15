9ef4e1a6c13c431596048d42bad4107904c187c3
deni1804 <denizulkarnaen223@gmail.com>
2022-06-15 21:39:28 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pantau/';
+//$config['base_url'] = 'http://localhost/pantau/';
+$config['base_url'] = '192.168.7.30/pantau';
+
 
 /*
 |--------------------------------------------------------------------------
