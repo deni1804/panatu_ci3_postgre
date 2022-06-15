46a2f8c0b25f7b2f35a7ab5966cb9ea20d252bf3
deni1804 <denizulkarnaen223@gmail.com>
2022-06-15 21:55:09 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -32,7 +32,7 @@ $config['base_url'] = 'http://192.168.7.30/pantau/';
 | Index File
 |--------------------------------------------------------------------------
 |
-| Typically this will be your index.php file, unless you've renamed it to
+| Typically this will be your gindex.php file, unless you've renamed it to
 | something else. If you are using mod_rewrite to remove the page set this
 | variable so that it is blank.
 |
