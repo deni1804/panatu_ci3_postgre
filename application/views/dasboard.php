7453d3ea7f5aff60a4adf8bfc089542410ddbae5
deni1804 <denizulkarnaen223@gmail.com>
2022-06-15 23:02:59 +0700

    coment

--- application/views/dasboard.php
+++ application/views/dasboard.php
@@ -329,7 +329,9 @@
                                 <div class="nav nav-tabs" id="nav-tab" role="tablist">
                                     <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Daily Report</a>
                                     <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Yesterday Report</a>
-                                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Monthly Report</a>
+                                    <!--  
+                                      <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Monthly Report</a>
+        -->
                                 </div>
                             </nav>
                         </ul>
@@ -388,6 +390,7 @@
                                 </table>
 
                             </div>
+                            <!--
                             <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
                                 <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
@@ -414,6 +417,7 @@
                                 </table>
 
                             </div>
+                                    -->
                         </div>
                     </div>
                 </div>
