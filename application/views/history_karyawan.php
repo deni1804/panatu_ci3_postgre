e015f94cd9976575d7f66e053787b51de7b0b200
deni1804 <denizulkarnaen223@gmail.com>
2022-09-21 19:01:01 +0700

    count repot

--- application/views/history_karyawan.php
+++ application/views/history_karyawan.php
@@ -2,7 +2,7 @@
     <div class="row">
 
         <!-- Tabel -->
-        <div class="col-xl-12">
+        <div class="col-xl-12 col-lg-7">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -36,7 +36,7 @@
                     </form>
                 </div>
                 <!-- Card Body -->
-                <div class="card-body">
+                <div class="card-body ">
                     <div class="table-responsive">
                         <div class="table-wrapper-scroll-y my-custom-scrollbar">
                             <table id="data" class="table table-bordered mb-0" style="width:100%">
@@ -80,6 +80,14 @@
                             </table>
                         </div>
                     </div>
+                </div>
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary"><?php $b = $i - 1;
+                                                                    $c = $b / 6;
+                                                                    echo $c;
+                                                                    ?> Repots</h6>
+
+
                 </div>
             </div>
         </div>
