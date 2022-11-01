be63eaefd6845d206725b026f668ab94c49ec39c
deni1804 <denizulkarnaen223@gmail.com>
2022-11-02 05:41:07 +0700

    tes git pull

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/pantau/';
-$config['base_url'] = 'http://192.168.7.31/pantau/';
+$config['base_url'] = 'http://192.168.7.311/pantau/';
 
 
 /*
