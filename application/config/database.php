96445bcd04b4277e8a2b903da17a71305d70969d
deni1804 <denizulkarnaen223@gmail.com>
2023-05-03 08:49:13 +0700

    database seting

--- application/config/database.php
+++ application/config/database.php
@@ -93,23 +93,23 @@ $db['default'] = array(
 
 
 	
+	
+
+
+		'hostname' => 'localhost',
+	'username' => 'postgres',
+	'password' => 'deni',
+	'dbdriver' => 'postgre',
+	'database' => 'pantau',
+
+	
+	*/
+
 	'hostname' => 'pgsql:host=localhost;dbname=pantau',
 	'username' => 'monitoring',
 	'password' => 'imaniprima11',
 	'dbdriver' => 'pdo',
 
-
-	
-
-	
-	*/
-
-
-	'hostname' => 'localhost',
-	'username' => 'postgres',
-	'password' => 'deni',
-	'dbdriver' => 'postgre',
-	'database' => 'pantau',
 
 	'dbprefix' => '',
 	'pconnect' => FALSE,
